5a955f36bea264f91e93f09979202d73f1a83e86
ebube victor <ebubev20@gmail.com>
2022-09-09 21:58:11 -0700

    ALX

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,10 +17,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	last_digit = n % 10;
 	if (last_digit > 5)
-		printf("last_digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("last digit of %d is %d and is greater than 5\n", n, last_digit);
 	else if (last_digit < 6 && last_digit != 0)
-		printf("last_digit of %d is %d and is less than 6 not 0\n", n, last_digit);
+		printf("last digit of %d is %d and is less than 6 not 0\n", n, last_digit);
 	else
-		printf("last_digit of %d is %d and is 0\n", n, last_digit);
+		printf("last digit of %d is %d and is 0\n", n, last_digit);
 	return (0);
 }
