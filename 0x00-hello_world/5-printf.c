b742d63e736c2077aedd98d9869eeab92575d5a8
ebube victor <ebubev20@gmail.com>
2022-09-12 16:03:32 -0700

    ALX

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prining
+ *
+ * Return: Always 0(successs)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	return (0);
+}
