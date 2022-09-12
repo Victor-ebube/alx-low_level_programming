6b5df302b872a6f062aeff4e443773969adc67ec
ebube victor <ebubev20@gmail.com>
2022-09-12 12:57:20 -0700

    ALX

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,9 +9,9 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
-		putchar((i % 16) + '0');
+		putchar((i % 10) + '0');
 	for (i = 'a'; i < 'g'; i++)
-		putchar(i)
+		putchar(i);
 	putchar('\n');
 
 	return (0);
