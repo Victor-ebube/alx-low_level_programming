8472543b9db216b421ed9e364829baf6d0908075
ebube victor <ebubev20@gmail.com>
2022-09-13 21:51:29 -0700

    ALX

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * main - prints alphabets in lower case
- * Return: always 0 success
+ * print_alphabet - prints alphabets in lower case
  */
 void print_alphabet(void)
 {
-	
 	char i;
 
-	for (i = 'a', i <= 'z', i++)
+	for (i = 'a'; i <= 'z'; i++)
+	{
 		_putchar(i);
+	}
 
 	_putchar('\n');
 
-	return (0);
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets x10 in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int x = 0;
+	char i;
+
+	while (x < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
