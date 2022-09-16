bd7f4be9096f3f07eeb38274f01afd5fa32c1542
ebube victor <ebubev20@gmail.com>
2022-09-15 17:00:35 -0700

    ALX

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - checks infinite loop
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	/*int i;*/
+
+	printf("infinite loop incoming :(\n");
+
+	/* i = 0;*/
+
+	/*while (i > 0)*/
+	/*{*/
+		/*putchar(i);*/
+	/*}*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
