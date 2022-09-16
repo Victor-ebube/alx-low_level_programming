277d18bbe9292cf37d42d629ecc81841e2c7bde0
ebube victor <ebubev20@gmail.com>
2022-09-16 04:52:30 -0700

    ALX

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,27 +11,27 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || year % 40 == 0)
+	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 		if (month == 2 && day <= 60)
 		{
-			printf("day of the year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 		else if (month >= 3 && day >= 60)
 		{
-			printf("day of the year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);
 		}
 	}
 	else
 	{
 		if (month == 2 && day == 60)
-			printf("invalid date: %2d/%2d/%04d\n", month, day - 31, year);
+			printf("Invalid date: %2d/%2d/%04d\n", month, day - 31, year);
 
 		else
 		{
-			printf("day of the year: %d\n", day);
+			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
